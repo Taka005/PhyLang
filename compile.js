94ce1491dafbd32410708a35e4b1c60508a76c35
Taka005 <01taka005@gmail.com>
2023-08-06 08:56:11 +0000

    修正

--- compile.js
+++ compile.js
@@ -1,24 +1,24 @@
 const fs = require("fs");
 
-console.log("JASコンパイラーが起動しました");
-console.log("ファイルのチェック中...");
+console.log("\x1b[34mJASコンパイラーが起動しました\x1b[39m");
+console.log("\x1b[34mファイルのチェック中...\x1b[39m");
 
 const FilePath = process.argv[2];
 if(!FilePath){
-  console.log("ファイルエラー: コンパイル先のJASファイルを指定してください");
+  console.log("\x1b[31mファイルエラー: コンパイル先のJASファイルを指定してください\x1b[39m");
   process.exit();
 }
 
 if(FilePath.split(".").pop() !== "jas"){
-  console.log("ファイルエラー: 拡張子が.jasのファイルを指定してください");
+  console.log("\x1b[31mファイルエラー: 拡張子が.jasのファイルを指定してください\x1b[39m");
   process.exit();
 }
 
 if(!fs.existsSync(FilePath)){
-  console.log(`ファイルエラー: ${file} は存在しません`);
+  console.log(`\x1b[31mファイルエラー: ${FilePath} は存在しません\x1b[39m`);
   process.exit();
 }
 
 const FileDate = fs.readFileSync(FilePath);
 
-console.log("ファイルのチェック完了");
+console.log("\x1b[34mファイルのチェック完了\x1b[39m");
